b59925688fadff885683b99565ec9d06db7355cc
Đường Trung Nguyên <duongtrungnguyenrc@gmail.com>
2025-05-15 21:01:52 +0700

    Add auth service

--- scripts/build-proto.sh
+++ scripts/build-proto.sh
@@ -1,5 +1,14 @@
 protoc \
   --plugin=./node_modules/.bin/protoc-gen-ts_proto \
-  --ts_proto_out=./src/common/protos --ts_proto_opt=nestJs=true \
-  --proto_path=./protos \
-  ./protos/*.proto
+  --ts_proto_out=./src/user-grpc/protos \
+  --ts_proto_opt=nestJs=true,useDate=true \
+  --proto_path=./src/user-grpc/protos \
+  ./src/user-grpc/protos/user.proto
+
+
+protoc \
+  --plugin=./node_modules/.bin/protoc-gen-ts_proto \
+  --ts_proto_out=./src/user-client/tsprotos \
+  --ts_proto_opt=nestJs=true,useDate=true \
+  --proto_path=./src/user-client/protos \
+  ./src/user-client/protos/user.proto
